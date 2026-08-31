793bd8ab417ecb9d673402b176e099344544192b
Richard <richardlapuente@hotmail.com>
2026-08-31 10:52:37 -0300

    fix: redireciona caches do laravel para /tmp no serverless

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Tratamento de caminhos para o ambiente Serverless da Vercel
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
