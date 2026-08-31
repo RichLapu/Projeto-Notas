23835198adf1b32143cff8af77e44a4a6eb7fef4
Richard <richardlapuente@hotmail.com>
2026-08-31 15:24:50 -0300

    fix: adiciona tag do favicon na head do main_layout

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -9,6 +9,7 @@
     <!-- FontAwesome via CDN Oficial (Resolve o problema dos ícones sumidos na Vercel) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.quilljs.com/1.3.7/quill.snow.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v=2" type="image/x-icon">
     <style>
     /* Força as imagens inseridas pelo Quill a serem responsivas */
     .card img, .ql-editor img {
