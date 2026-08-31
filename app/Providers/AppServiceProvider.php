afac8d728f7f9620f0b12d900da96a329ac3ee58
Richard <richardlapuente@hotmail.com>
2026-08-31 11:55:11 -0300

    fix: força o uso de https para os assets em producao

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Adicione esta linha
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Força HTTPS em ambiente de produção (Vercel)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
